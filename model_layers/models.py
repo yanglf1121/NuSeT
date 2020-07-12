a0dbd0537f5f6bc82838553c8003a500af5db3bb
Linfeng Yang <42359578+yanglf1121@users.noreply.github.com>
2020-07-12 10:48:34 +0800

    Update models.py
    Correct the outdated comments

--- model_layers/models.py
+++ model_layers/models.py
@@ -1,55 +1,17 @@
 import tensorflow as tf
 from utils.tf_utils import activation_fun
 
-def compile_model_FC(genome, nb_classes, inputs):
-    """Compile a sequential model.
-
-    Args:
-        genome (dict): the parameters of the genome
-
-    Returns:
-        a compiled network.
-
-    """
-    # Get our network parameters.
-    nb_layers  = genome.geneparam['nb_layers' ]
-    nb_neurons = genome.nb_neurons()
-    activation = genome.geneparam['activation']
-    optimizer  = genome.geneparam['optimizer' ]
-    learning_rate = genome.geneparam['learning_rate' ]
-    logging.info("Architecture:%s,%s,%s,%d,%f" % (nb_neurons[0:nb_layers], activation, optimizer, nb_layers, learning_rate))
-    padding = 'same'
-    # Add each layer.
-    for i in range(0,nb_layers):
-        # Need input shape for first layer.
-        if i == 0:
-            outputs = tf.layers.conv2d(inputs, nb_neurons[i], 3, padding=padding, kernel_initializer=tf.contrib.layers.xavier_initializer(), name='conv'+str(i), use_bias=True)
-            
-        else:
-            outputs = tf.layers.conv2d(outputs, nb_neurons[i], 3, padding=padding, kernel_initializer=tf.contrib.layers.xavier_initializer(), name='conv'+str(i), use_bias=True)
-        
-        outputs = tf.layers.batch_normalization(outputs)
-        outputs = activation_fun(outputs,activation)
-        outputs = tf.nn.dropout(outputs,0.2)
-    # only output 2 featuremap at the end
-    outputs = tf.layers.conv2d(outputs, nb_classes, 3, padding=padding, kernel_initializer=tf.contrib.layers.xavier_initializer(), name='final', use_bias=False)
-    
-    return outputs
-
-
 def UNET(nb_classes, inputs):
     """Compile a UNET model.
 
     Args:
-        genome (dict): the parameters of the genome
+        nb_classes: the number of classes to predict
+        inputs: the input tensor
 
     Returns:
-        a compiled network.
+        an output tensor, with 'nb_classes' of featuremaps
 
     """
-    # Get our network parameters.
-    #nb_layers  = genome.geneparam['nb_layers' ]
-    #nb_neurons = genome.nb_neurons()
 
     padding = 'same'
     # Conv block 1
@@ -75,7 +37,6 @@ def UNET(nb_classes, inputs):
     
     # Down-sample 2
     outputs = tf.layers.max_pooling2d(outputs,pool_size = 2,strides = 2,padding=padding)
-    # Now is ?,112,112,128
     
     # Conv block 3
     outputs = tf.layers.conv2d(outputs, 256, 3, padding=padding, kernel_initializer=tf.contrib.layers.xavier_initializer(), name='conv3-1', use_bias=True)
